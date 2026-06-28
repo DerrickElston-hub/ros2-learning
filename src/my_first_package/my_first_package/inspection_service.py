10005b6d908ccede5c3975c4067d72899f4f45d1
Derrick Elston H <derrickelston37@gmail.com>
2026-06-28 14:08:26 +0530

    Add custom ROS2 service with Inspection.srv

--- src/my_first_package/my_first_package/inspection_service.py
+++ src/my_first_package/my_first_package/inspection_service.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
-from std_srvs.srv import Trigger
+
+from my_robot_interfaces.srv import Inspection
 
 
 class InspectionService(Node):
@@ -9,20 +10,22 @@ class InspectionService(Node):
         super().__init__('inspection_service')
 
         self.srv = self.create_service(
-            Trigger,
-            'start_inspection',
+            Inspection,
+            'inspect_part',
             self.start_callback
         )
 
-        self.get_logger().info("Inspection Service Ready")
-
+        self.get_logger().info("Custom Inspection Service Ready")
 
     def start_callback(self, request, response):
 
-        self.get_logger().info("Inspection Started")
+        self.get_logger().info(
+            f"Inspecting Part ID: {request.part_id}"
+        )
 
-        response.success = True
-        response.message = "Inspection Started Successfully"
+        response.passed = True
+        response.confidence = 0.96
+        response.message = "Inspection Passed"
 
         return response
 
